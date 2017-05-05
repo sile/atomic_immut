68039fcfc0efdc7550a8f246156658168a3b0989
Takeru Ohta <phjgt308@gmail.com>
2017-05-06 01:31:38 +0900

    Add benchmarks

--- benches/lib.rs
+++ benches/lib.rs
@@ -5,6 +5,7 @@ extern crate test;
 
 use std::sync::{Arc, Barrier};
 use std::thread;
+use std::time::Duration;
 use atomic_immut::AtomicImmut;
 use test::Bencher;
 
@@ -27,45 +28,97 @@ fn single_thread_load_std(b: &mut Bencher) {
 #[bench]
 fn multi_thread_load(b: &mut Bencher) {
     let v = Arc::new(AtomicImmut::new(vec![0, 1, 2]));
-    let thread_count = 16;
+    let thread_count = 8;
     let barrier = Arc::new(Barrier::new(thread_count));
     for _ in 0..thread_count {
         let v = v.clone();
         let barrier = barrier.clone();
         thread::spawn(move || {
-                          while !v.load().is_empty() {
-                              //thread::sleep_ms(1);
-                          }
+                          while !v.load().is_empty() {}
                           barrier.wait();
                       });
     }
-    thread::sleep_ms(10);
+    thread::sleep(Duration::from_millis(10));
     b.iter(|| { test::black_box(v.load()); });
     v.store(vec![]);
     barrier.wait();
-    assert!(v.load().is_empty());
     assert_eq!(Arc::strong_count(&v.load()), 2);
 }
 
 #[bench]
 fn multi_thread_load_std(b: &mut Bencher) {
     let v = Arc::new(StdAtomicImmut::new(vec![0, 1, 2]));
-    let thread_count = 16;
+    let thread_count = 8;
     let barrier = Arc::new(Barrier::new(thread_count));
     for _ in 0..thread_count {
         let v = v.clone();
         let barrier = barrier.clone();
         thread::spawn(move || {
-                          while !v.load().is_empty() {
-                              //thread::sleep_ms(1);
+                          while !v.load().is_empty() {}
+                          barrier.wait();
+                      });
+    }
+    thread::sleep(Duration::from_millis(10));
+    b.iter(|| { test::black_box(v.load()); });
+    v.store(vec![]);
+    barrier.wait();
+    assert_eq!(Arc::strong_count(&v.load()), 2);
+}
+
+#[bench]
+fn multi_thread_store_and_load(b: &mut Bencher) {
+    let v0 = Arc::new(AtomicImmut::new(vec![0, 1, 2]));
+    let v1 = Arc::new(AtomicImmut::new(0));
+    let thread_count = 4;
+    let barrier = Arc::new(Barrier::new(thread_count));
+    for _ in 0..thread_count {
+        let v0 = v0.clone();
+        let v1 = v1.clone();
+        let barrier = barrier.clone();
+        thread::spawn(move || {
+                          while !v0.load().is_empty() {
+                              v1.store(1);
                           }
                           barrier.wait();
                       });
     }
-    thread::sleep_ms(10);
-    b.iter(|| { test::black_box(v.load()); });
-    v.store(vec![]);
+    thread::sleep(Duration::from_millis(10));
+    b.iter(|| {
+               test::black_box(v0.load());
+               test::black_box(v1.load());
+           });
+    v0.store(vec![]);
     barrier.wait();
-    assert!(v.load().is_empty());
-    assert_eq!(Arc::strong_count(&v.load()), 2);
+    assert_eq!(Arc::strong_count(&v0.load()), 2);
+    assert_eq!(Arc::strong_count(&v1.load()), 2);
+    assert_eq!(*v1.load(), 1);
+}
+
+#[bench]
+fn multi_thread_store_and_load_std(b: &mut Bencher) {
+    let v0 = Arc::new(StdAtomicImmut::new(vec![0, 1, 2]));
+    let v1 = Arc::new(StdAtomicImmut::new(0));
+    let thread_count = 4;
+    let barrier = Arc::new(Barrier::new(thread_count));
+    for _ in 0..thread_count {
+        let v0 = v0.clone();
+        let v1 = v1.clone();
+        let barrier = barrier.clone();
+        thread::spawn(move || {
+                          while !v0.load().is_empty() {
+                              v1.store(1);
+                          }
+                          barrier.wait();
+                      });
+    }
+    thread::sleep(Duration::from_millis(10));
+    b.iter(|| {
+               test::black_box(v0.load());
+               test::black_box(v1.load());
+           });
+    v0.store(vec![]);
+    barrier.wait();
+    assert_eq!(Arc::strong_count(&v0.load()), 2);
+    assert_eq!(Arc::strong_count(&v1.load()), 2);
+    assert_eq!(*v1.load(), 1);
 }
